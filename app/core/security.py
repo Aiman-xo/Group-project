b0b9a21c268a524d3cba3305400fca4972fbacd4
Faheem Shan <faheemka246@gmail.com>
2026-06-02 16:34:43 +0530

    registration

--- app/core/security.py
+++ app/core/security.py
@@ -1,1 +1,17 @@
 # THIS IS WHERE WE WRITE THE ACCESS TOKEN CREATION AND REFRESH TOKEN CREATION AND PASSWORD HASHING CODE.
+
+from passlib.context import CryptContext
+
+# bcrypt hashing algorithm
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def hash_password(password: str):
+    """
+    Convert plain password to hashed password
+    """
+
+    return pwd_context.hash(password)
