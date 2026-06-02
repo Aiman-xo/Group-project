417be9bcef5f2a933f0f936a4530d0ea14208364
Faheem Shan <faheemka246@gmail.com>
2026-06-02 16:34:43 +0530

    registration

---
+++ app/schemas/authentication_schema.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel,EmailStr
+
+class CompanyRegister(BaseModel):
+
+    email: EmailStr
+    company_name: str
+    website_link: str | None = None
+    industry: str
+    password: str
