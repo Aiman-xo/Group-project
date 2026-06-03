12f6675f1c45afc2f54072493a35222066483f9f
Aiman-xo <aimanrasack@gmail.com>
2026-06-03 17:49:50 +0530

    configured upstash and fastapi redis connection

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+import redis.asyncio as redis
+from contextlib import asynccontextmanager
+from app.core.redis_config import init_redis_pool,redis_pool
 from app.routes.authentication_routes import router as auth_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    # Boot up the pool
+    init_redis_pool()
+    yield
+    # Shut down the pool
+    if redis_pool:
+        await redis_pool.disconnect()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router)
