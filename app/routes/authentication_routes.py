51e6da40b81eb45444f27acfb1a5ce242bdbd7f6
Adil-Aadhi <adilshinasmongam@gmail.com>
2026-06-04 14:00:17 +0530

    finished redis in registration

--- app/routes/authentication_routes.py
+++ app/routes/authentication_routes.py
@@ -5,21 +5,30 @@ from app.core.database import get_db
 from app.schemas.authentication_schema import CompanyRegister,LoginRequest
 from app.service.authentication_service import register_company,login_company
 
+from app.core.redis_config import get_redis
+from redis.asyncio import Redis
+
 router=APIRouter(
     prefix="/auth",
     tags=["authentication"]
 )
 
 @router.post("/register")
-def register(
+async def register(
     company: CompanyRegister,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    redis_client: Redis = Depends(get_redis)
 ):
-    return register_company(
+    return await register_company(
         company_data=company,
-        db=db
+        db=db,
+        redis_client=redis_client
     )
 
 @router.post("/login")
-def login(login_data:LoginRequest,db:Session=Depends(get_db)):
-    return login_company(login_data,db)
+async def login(
+    login_data:LoginRequest,db:Session=Depends(get_db),redis_client: Redis = Depends(get_redis)
+):
+    return await login_company(
+        login_data,db,redis_client=redis_client
+    )
