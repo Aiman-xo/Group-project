b2947cd8919d5df7c3ea122dc7b1d60878d3d005
Aiman-xo <aimanrasack@gmail.com>
2026-06-05 12:08:39 +0530

    fixed OTP generation issue

--- app/main.py
+++ app/main.py
@@ -4,6 +4,11 @@ from contextlib import asynccontextmanager
 from app.core.redis_config import init_redis_pool,redis_pool
 from app.routes.authentication_routes import router as auth_router
 
+# Rate Limiting
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+from app.core.limiter import limiter
+
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     # Boot up the pool
@@ -14,5 +19,8 @@ async def lifespan(app:FastAPI):
         await redis_pool.disconnect()
 
 app = FastAPI(lifespan=lifespan)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.include_router(auth_router)
