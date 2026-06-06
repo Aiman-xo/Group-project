639119782192a8c43a48a26a87c5f20766a2f62e
Faheem Shan <faheemka246@gmail.com>
2026-06-06 13:40:27 +0530

    Add company registration validation and authentication updates

--- app/service/authentication_service.py
+++ app/service/authentication_service.py
@@ -1,11 +1,12 @@
 from sqlalchemy.orm import Session
 
 from app.models.company_model import Company
-from app.schemas.authentication_schema import CompanyRegister,LoginRequest
+from app.schemas.authentication_schema import CompanyRegister,LoginRequest,ForgotPasswordRequest,VerifyForgotPasswordOTPRequest,ResetPasswordRequest
 
 from app.core.security import hash_password,create_access_token,create_refresh_token,verify_password
+from app.core.validators import validate_password_strength
 
-from app.service.otp_service import generate_otp
+from app.service.otp_service import generate_otp,generate_forgot_password_otp,verify_forgot_password_otp
 from app.utils.email import send_otp_email
 from app.core.session import generate_session_id
 
@@ -32,7 +33,16 @@ async def register_company(
     if existing_company:
         raise HTTPException(status_code=400,detail='Email exists Try other one!')
 
+    if company_data.password != company_data.confirm_password:
+        raise HTTPException(
+            status_code=400,
+            detail="Password and confirm password do not match"
+        )
     
+    validate_password_strength(
+        company_data.password
+    )
+
     # Hash password
     hashed_password = hash_password(
         company_data.password
@@ -109,6 +119,99 @@ async def login_company(login_data:LoginRequest,db:Session,redis_client: Redis):
         "token_type":"bearer",
         "schema_name":company.schema_name
     }
-def forget_password(l):
-    pass
 
+
+async def forgot_password(
+    payload: ForgotPasswordRequest,
+    db: Session,
+    redis_client: Redis,
+    background_tasks: BackgroundTasks
+):
+
+    company = db.query(Company).filter(
+        Company.email == payload.email
+    ).first()
+
+    if not company:
+        raise HTTPException(
+            status_code=404,
+            detail="Email not found"
+        )
+
+    otp = await generate_forgot_password_otp(
+        payload.email,
+        redis_client
+    )
+
+    await send_otp_email(
+        payload.email,
+        otp,
+        background_tasks
+    )
+
+    return {
+        "message":"Password reset OTP sent"
+    }
+
+async def verify_forgot_password(
+    payload: VerifyForgotPasswordOTPRequest,
+    redis_client: Redis
+):
+    return await verify_forgot_password_otp(
+        payload.email,
+        payload.otp,
+        redis_client
+    )
+
+async def reset_password(
+    payload: ResetPasswordRequest,
+    db: Session,
+    redis_client: Redis
+):
+
+    allowed = await redis_client.get(
+        f"reset_allowed:{payload.email}"
+    )
+
+    if not allowed:
+        raise HTTPException(
+            status_code=400,
+            detail="OTP verification required"
+        )
+
+    company = db.query(Company).filter(
+        Company.email == payload.email
+    ).first()
+
+    if not company:
+        raise HTTPException(
+            status_code=404,
+            detail="Company not found"
+        )
+
+    if payload.new_password != payload.confirm_password:
+        raise HTTPException(
+            status_code=400,
+            detail="New password and confirm password do not match"
+        )
+    
+    if verify_password(payload.new_password,company.password):
+        raise HTTPException(
+            status_code=400,
+            detail="New password cannot be the same as old password"
+        )
+
+    company.password = hash_password(
+        payload.new_password
+    )
+
+
+    db.commit()
+
+    await redis_client.delete(
+        f"reset_allowed:{payload.email}"
+    )
+
+    return {
+        "message":"Password updated successfully. Please login again."
+    }
